b240bb9bd7134ed2f6862a4a71a16636308629ff
eomao <156084917+eomao@users.noreply.github.com>
2026-03-20 12:22:41 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int average(int count, ...){
+int average(int count, ...) {
     va_list args;
     int i;
     int sum = 0;
-    for(i = 0; i < count; i++){
+
+    va_start(args, count);
+
+    for (i = 0; i < count; i++) {
         sum += va_arg(args, int);
     }
+
     va_end(args);
-    return sum/count;
+
+    return sum / count;
 }
 
-int main(){
+int main() {
     printf("%d\n", average(3, 10, 20, 30));
     printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
