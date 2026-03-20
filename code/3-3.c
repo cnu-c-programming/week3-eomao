e439a2ba1c7961445508ec57976816744835549d
eomao <156084917+eomao@users.noreply.github.com>
2026-03-20 12:14:58 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-int main() {
+int average(int count, int* nums){
+    int i;
+    int sum = 0;
+    for(i = 0; i < count; i++){
+        sum += nums[i];
+    }
+    return sum/count;
+}
 
+int main(){
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
     return 0;
 }
