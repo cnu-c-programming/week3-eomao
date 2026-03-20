f2704489d69088c499fa91724972d56d8986dd2f
eomao <156084917+eomao@users.noreply.github.com>
2026-03-20 12:19:39 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int max_of(int count, int* num){
-    int max = 0;
-    int i;
-    for(i = 0; i < count; i++){
-        if(num[i] > max){
-            max = num[i];
+int max_of(int count, ...) {
+    va_list args;
+    int i, max;
+
+    va_start(args, count);
+
+    max = va_arg(args, int);   // 첫 번째 값을 먼저 최댓값으로 설정
+
+    for (i = 1; i < count; i++) {
+        int num = va_arg(args, int);
+        if (num > max) {
+            max = num;
         }
     }
+
+    va_end(args);
+
     return max;
 }
 
-int main(){
+int main() {
     printf("%d\n", max_of(4, 5, 12, 3, 9));
     printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
     printf("%d\n", max_of(3, 100, 200, 300));
